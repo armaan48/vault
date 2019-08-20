f2011b8aaa09d8486b40a6208acb05507b4e0e95
armaan84 <54315881+armaan84@users.noreply.github.com>
2019-08-20 17:33:25 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number--- "))
+ctr=a
+while ctr!=0:
+    b=ctr
+    while b!=0:
+        print("**",end="  ")
+        b=b-1
+    print("""
+    """)    
+    ctr=ctr-1
+print("Thank you")
